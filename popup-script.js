9c62fb8554f2d8e0e9502c97a27afd55ca85251c
SleekPanther <macrobyte2@gmail.com>
2018-02-01 21:34:02 -0500

    Handle Enter key & move to separate function

--- popup-script.js
+++ popup-script.js
@@ -1,21 +1,17 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
 	$('#populateNetidButton').on('click', (e)=>{
-		chrome.tabs.query({active: true, currentWindow: true}, (tabs)=> {
-			let netId = document.getElementById('netId').value.trim()
-			chrome.storage.sync.set({
-				'netId': netId
-			})
-			chrome.tabs.sendMessage(
-				tabs[0].id,		//tabs[0] just in case multiple current tabs
-				{
-					msg: 'saveNetId', 
-					netId: netId
-				}
-			)
-		})
+		sendNetIdToContentScript()
 	})
 
+	//Handle enter key
+	$('#netId').on('keydown', (e)=> {
+		if (e.which == 13) {
+			sendNetIdToContentScript()
+		}
+	})
+
+	//Auto-populate field in popup with synced value when popup opens
 	chrome.storage.sync.get(['netId'], (syncedData)=> {
 		if(chrome.runtime.lastError){
 			console.log(chrome.runtime.lastError.message)
@@ -27,3 +23,19 @@ document.addEventListener("DOMContentLoaded", ()=>{
 	})
 
 })
+
+function sendNetIdToContentScript(){
+	chrome.tabs.query({active: true, currentWindow: true}, (tabs)=> {
+		let netId = document.getElementById('netId').value.trim()
+		chrome.storage.sync.set({
+			'netId': netId
+		})
+		chrome.tabs.sendMessage(
+			tabs[0].id,		//tabs[0] just in case multiple current tabs
+			{
+				msg: 'saveNetId', 
+				netId: netId
+			}
+		)
+	})
+}
